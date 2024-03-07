67008ee4350e25a273910af696be4bd7da8ca36c
khushpreet <khushp961@gmail.com>
2024-03-07 10:58:34 +0530

    changed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require('ejs-mate');
 
 const mongoose = require("mongoose");
 const { log } = require("console");
@@ -22,6 +23,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
 
 app.get("/", (req, res) => {
   res.send("this is working");
